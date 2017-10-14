1ea68ae7eaa7aca03f77f5f49977bec0bd0df806
Sam Messina <samuel.messina@gmail.com>
2017-10-14 12:02:28 -0500

    more refactors

--- lab3/lab3-server.c
+++ lab3/lab3-server.c
@@ -27,19 +27,19 @@
 #define INPUT_SIZE 4096
 #define MAX_EVENTS 20
 
-int    setup_server(void);
-void * epoll_wait_loop(void*arg);
-int    transfer_data(int in_fd, int out_fd);
-int    close_hung_fds(int hung_fd);
-int    accept_new_client(int server_sockfd);
-void * handle_client(void*client_sockfd_ptr);
-void   timer_handler (int sig, siginfo_t *si, void *uc);
-int    setup_timer(int client_sockfd);
-int    handle_protocol(int client_sockfd);
-int    exec_bash(char  *slave_pty_name);
-int    setup_client_pty_epoll_units(int client_sockfd, int master_pty_fd);
-int    setup_pty(void);
-
+int     setup_server(void);
+void *  epoll_wait_loop(void*arg);
+int     transfer_data(int in_fd, int out_fd);
+int     close_hung_fds(int hung_fd);
+int     accept_new_client(int server_sockfd);
+void *  handle_client(void*client_sockfd_ptr);
+void    timer_handler (int sig, siginfo_t *si, void *uc);
+timer_t setup_timer(int client_sockfd);
+int     handle_protocol(int client_sockfd);
+int     exec_bash(char  *slave_pty_name);
+int     setup_client_pty_epoll_units(int client_sockfd, int master_pty_fd);
+int     setup_pty(void);
+ 
 int epoll_fds[MAX_EVENTS*2 + 5];
 int epfd;
 
@@ -48,18 +48,25 @@ int main()
     int        server_sockfd;     // socket fd for the server
     int        client_sockfd = 0; // client socket fd returned from accept()
     int       *client_sockfd_ptr; // memory to hold the client socket
-    pthread_t epoll_threadid;     // epoll wait loop thread
     pthread_t temp_threadid;      // handle new client protocol thread
+    pthread_t epoll_threadid;     // epoll wait loop thread
 
     // setup our server
-    server_sockfd = setup_server();
+    if ((server_sockfd = setup_server()) == -1) {
+        exit(EXIT_FAILURE);
+    }
 
     // create our epoll fd
-    epfd = epoll_create1(EPOLL_CLOEXEC);
+    if ((epfd = epoll_create1(EPOLL_CLOEXEC)) == -1) {
+        perror("Could not create epoll unit\n");
+        exit(EXIT_FAILURE);
+    }
 
     // start the epoll wait loop
     if (pthread_create(&epoll_threadid, NULL, epoll_wait_loop, NULL) == 
             (EAGAIN || EINVAL || EPERM)) {
+        perror("Could not start epoll_wait thread\n");
+        exit(EXIT_FAILURE);
     }
 
     // ignore bash's signals, as we don't care about exit status
@@ -67,9 +74,9 @@ int main()
 
     while(1) {
         // server set up and started
-    #ifdef DEBUG
+        #ifdef DEBUG
         printf("server waiting\n");
-    #endif
+        #endif
 
         /*
          * Create a little memory for our client socket FDs.
@@ -82,8 +89,7 @@ int main()
         // accept a new client
         client_sockfd = accept_new_client(server_sockfd);
 
-        // TODO this might be a gross way to throw away clients
-        // Look into alternatives.
+        // if accept fails, just continue accepting new clients
         if (client_sockfd < 0) {continue;};
 
         // get that file descriptor and put it in the 
@@ -91,10 +97,15 @@ int main()
         *client_sockfd_ptr = client_sockfd;
 
         // create a temporary thread to start handling the protocol
-        pthread_create(&temp_threadid, NULL, handle_client, client_sockfd_ptr);
+        if (pthread_create(&temp_threadid, NULL, handle_client, client_sockfd_ptr) == 
+                (EAGAIN || EINVAL || EPERM)) {
+            perror("Could not create temp thread for  client\n");
+            close(*client_sockfd_ptr);
+        }
 
         // ... and start it all again
     }
+    close(server_sockfd);
     return EXIT_FAILURE;
 }
 
@@ -104,10 +115,9 @@ int setup_server(void)
      * server off the ground
      */
     int       server_sockfd;
-    int       client_sockfd = 0;
     socklen_t server_len;
     struct    sockaddr_in server_address;
-    int option = 1;
+    int       option = 1;
 
     server_sockfd                  = socket(AF_INET, SOCK_STREAM, 0);
     server_address.sin_family      = AF_INET;
@@ -118,40 +128,49 @@ int setup_server(void)
 
     // use address for socket address to avoid pass by value
     if (bind(server_sockfd, (struct sockaddr *)&server_address, server_len)< 0) {
-        perror("could not bind server");
-        exit(1);
+        perror("Error. Could not bind server\n");
+        return -1;
     }
 
     // listen with a backlog of 5 connections
     if ((listen(server_sockfd, 5) < 0)) { 
-        perror("Oops. Error \n"); 
-        close(client_sockfd);
+        perror("Oops. Error listening on server\n"); 
+        return -1;
     }
-
     return server_sockfd;
 }
 
+
 void * epoll_wait_loop(void*arg)
 {
-    int ready;
-    int sfd;
-    int i;
+    int    ready; // how many ready epoll events
+    int    sfd;   // the fd of the ready epoll event
+    int    i;     // counter for for loop
     struct epoll_event evlist[MAX_EVENTS];
+    struct epoll_event current_event;
+
     while (1) {
         ready = epoll_wait(epfd, evlist, MAX_EVENTS, -1);
-        for (i = 0; i < ready; i++) {
 
-            if ((evlist[i].events & EPOLLHUP) || 
-                    (evlist[i].events & EPOLLERR) ) {
-                // in the event of error...
-                // get the error'ed file descriptor (sfd)
-                sfd = evlist[i].data.fd;
+        // go through all the ready events
+        for (i = 0; i < ready; i++) {
+            current_event = evlist[i];
+            // get the fd of the ready event
+            sfd = current_event.data.fd;
+
+            if ((current_event.events & EPOLLHUP) || 
+                    (current_event.events & EPOLLERR) ) {
+                // in the event of error,
+                // close both the hungup fd and its compatriot
                 close_hung_fds(sfd);
                 close_hung_fds(epoll_fds[sfd]);
             }
-            if (evlist[i].events & EPOLLIN) {
-                sfd = evlist[i].data.fd;
-                printf("epoll fd: %d\n", sfd);
+            if (current_event.events & EPOLLIN) {
+                #ifdef DEBUG
+                printf("Ready epoll fd: %d\n", sfd);
+                #endif
+
+                // transfer data fromthe ready epoll event to its compatriot
                 if (transfer_data(sfd, epoll_fds[sfd]) <= 0) {
                     close_hung_fds(sfd);
                     close_hung_fds(epoll_fds[sfd]);
@@ -163,10 +182,13 @@ void * epoll_wait_loop(void*arg)
 
 int transfer_data(int in_fd, int out_fd)
 {
-    char buf[INPUT_SIZE];
-    int bytes_read;
-    memset(buf,0, sizeof(buf));
+    /* Transfer data from one fd to another */
 
+    char buf[INPUT_SIZE]; // holds the read data
+    int  bytes_read;      // holds return value of read()
+    memset(buf, 0, sizeof(buf));
+
+    // read from the in fd
     bytes_read = read(in_fd, buf, sizeof(buf));
     if (bytes_read == -1) {
         fprintf(stderr, "Error reading from: %d\n", in_fd);
@@ -176,6 +198,7 @@ int transfer_data(int in_fd, int out_fd)
         return 0;
     }
 
+    // write to the out fd
     bytes_read = write(out_fd, buf, strlen(buf));
     if (bytes_read == -1) {
         fprintf(stderr, "Error writing to : %d\n", out_fd);
@@ -186,20 +209,21 @@ int transfer_data(int in_fd, int out_fd)
 
 int close_hung_fds(int hung_fd)
 {
+
     // close the hung thread 
-    printf("closing %d\n", hung_fd);
-    if (close(hung_fd) == -1 ) {
     #ifdef DEBUG
-        printf("error closing fd: %d\n", hung_fd);
+    printf("closing %d\n", hung_fd);
     #endif
+    if (close(hung_fd) == -1 ) {
+        #ifdef DEBUG
+        printf("error closing fd: %d\n", hung_fd);
+        #endif
         return -1;
     }
-    else {
     #ifdef DEBUG
-        printf("closed fd: %d\n", hung_fd);
+    printf("closed fd: %d\n", hung_fd);
     #endif
-        return 1;
-    }
+    return 1;
 }
 
 int accept_new_client(int server_sockfd)
@@ -207,7 +231,7 @@ int accept_new_client(int server_sockfd)
     /* Accepts new clients and returns 
      * their FD
      */
-    int       client_sockfd = 0;
+    int       client_sockfd = 0; // socket for new client
     socklen_t client_len;
     struct    sockaddr_in client_address;
 
@@ -236,14 +260,18 @@ void * handle_client(void*client_sockfd_ptr)
      *
      * client_sockfd is the file descriptor for the socket.
      */
-    int client_sockfd = *(int*)client_sockfd_ptr;
-    free(client_sockfd_ptr);
-    int   master_pty_fd;
-    int   return_val;
-    char  *slave_pty_name;
-    pid_t pid;
+    int     client_sockfd; // socket for the client
+    int     master_pty_fd; // the master pty
+    char *  slave_pty_name;
+    pid_t   pid;
+    timer_t timerid;
 
-    if (setup_timer(client_sockfd) == -1) {
+    // get the client's fd from the malloc'ed memory
+    client_sockfd = *(int*)client_sockfd_ptr;
+    free(client_sockfd_ptr);
+
+    // start the timer
+    if ((timerid = setup_timer(client_sockfd)) == NULL) {
         close(client_sockfd);
         pthread_exit(NULL);
     }
@@ -256,6 +284,8 @@ void * handle_client(void*client_sockfd_ptr)
         close(client_sockfd);
         pthread_exit(NULL);
     }
+
+    timer_delete(timerid);
 
     // run all the setup functions for the master pty
     if ((master_pty_fd = setup_pty()) < 0) {
@@ -287,11 +317,10 @@ void * handle_client(void*client_sockfd_ptr)
             exec_bash(slave_pty_name);
         default: // parent
             // set up the epoll units and kill the temp thread
-            return_val = setup_client_pty_epoll_units(client_sockfd, master_pty_fd);
-            if (return_val < 0) {
+            if(setup_client_pty_epoll_units(client_sockfd, master_pty_fd) < 0) {
+                perror("setup_client_pty_epoll_units returned error\n");
                 kill(pid, SIGTERM);
                 pthread_exit(NULL);
-                exit(1);
             }
             pthread_exit(NULL);
     }
@@ -303,45 +332,50 @@ void timer_handler (int sig, siginfo_t *si, void *uc)
     * (int *) si->si_ptr = 1;
 }
 
-int setup_timer(int client_sockfd)
+timer_t setup_timer(int client_sockfd)
 {
-    struct itimerspec timer;
-    struct sigaction sa;
-    struct sigevent sev;
-    timer_t timerid;
-    int timer_flag = 0;
+    /* Set up our timer. The timer will count 3 seconds before 
+     * disconnecting an unresponsive client. Once the protocol is 
+     * complete, the timer is deleted
+     */
 
-    timer.it_value.tv_sec = 3;
-    timer.it_value.tv_nsec = 0;
-    timer.it_interval.tv_sec = 0;
+    struct itimerspec timer; // the timer itself
+    struct sigaction  sa;    // action to be taken on timer expiration
+    struct sigevent   sev;   // the even to the triggered
+    timer_t timerid;         // id of our timer
+    int     timer_flag = 0;  // flag to be switched when the timer completes
+
+    timer.it_value.tv_sec     = 3;
+    timer.it_value.tv_nsec    = 0;
+    timer.it_interval.tv_sec  = 0;
     timer.it_interval.tv_nsec = 0;
 
-    sa.sa_flags = SA_SIGINFO;
+    sa.sa_flags     = SA_SIGINFO;
     sa.sa_sigaction = timer_handler;
 
     sigemptyset(&sa.sa_mask);
 
     if (sigaction(SIGALRM, &sa, NULL) == -1) {
         perror("Error setting sigaction\n");
-        return -1;
+        return NULL;
     }
 
-    sev.sigev_signo = SIGALRM;
-    sev.sigev_notify = SIGEV_THREAD_ID;
+    sev.sigev_signo           = SIGALRM;
+    sev.sigev_notify          = SIGEV_THREAD_ID;
     sev.sigev_value.sival_ptr = &timer_flag;
-    sev._sigev_un._tid = syscall(SYS_gettid);
+    sev._sigev_un._tid        = syscall(SYS_gettid);
 
     if (timer_create(CLOCK_REALTIME, &sev, &timerid) == -1) {
         perror("Error creating timer\n");
-        return -1;
+        return NULL;
     }
 
     if (timer_settime(timerid, 0, &timer, NULL) == -1) {
         perror("Error setting time on timer\n");
-        return -1;
+        return NULL;
     }
 
-    return 1;
+    return timerid;
 }
 
 int handle_protocol(int client_sockfd) 
@@ -363,7 +397,9 @@ int handle_protocol(int client_sockfd)
         return -1;
     }
 
-    // wait to read the secret word from client
+    // wait to read the secret word from client.
+    // our timer will interrupt this read in the event of 
+    // a malicious client.
     if ((bytes_read = read(client_sockfd, buffer, sizeof(buffer)))< 0){
         perror("Oops. Error reading from client.\n"); 
         return -1;
@@ -436,7 +472,7 @@ int setup_client_pty_epoll_units(int client_sockfd, int master_pty_fd)
     ev.events  = EPOLLIN;
     ev.data.fd = client_sockfd;
     if (epoll_ctl(epfd, EPOLL_CTL_ADD, client_sockfd, &ev) == -1){
-        perror("epoll_ctl failed");
+        fprintf(stderr, "adding client epoll_ctl failed. fd: %d\n", client_sockfd);
         return -1;
     }
 
@@ -446,7 +482,7 @@ int setup_client_pty_epoll_units(int client_sockfd, int master_pty_fd)
     ev.events  = EPOLLIN;
     ev.data.fd = master_pty_fd;
     if (epoll_ctl(epfd, EPOLL_CTL_ADD, master_pty_fd, &ev) == -1) {
-        perror("epoll_ctl");
+        fprintf(stderr, "adding client epoll_ctl failed. fd: %d\n", master_pty_fd);
         return -1;
     }
 
